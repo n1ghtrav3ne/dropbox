cd9189b747f6930ed8f7fbaf6cf2e7e518f58b6f
C0DeB100D <c0deb100d.me@gmail.com>
2025-07-09 23:15:11 +0330

    created plugin for client

---
+++ plugins/gsap.client.ts
@@ -0,0 +1,16 @@
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
+
+export default defineNuxtPlugin((nuxtApp) => {
+    if(process.client) 
+    {
+        gsap.registerPlugin(ScrollTrigger)
+    }
+
+    return {
+        provide:{
+            gsap,
+            ScrollTrigger
+        }
+    }
+})
